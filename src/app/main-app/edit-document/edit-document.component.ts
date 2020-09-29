1344038475d40d36f018c66615d246ba824b8086
Amir Solnik <Amir.Solnik@gmail.com>
2020-09-29 18:08:20 +0300

    minor refactoring to edit-document-component

--- src/app/main-app/edit-document/edit-document.component.ts
+++ src/app/main-app/edit-document/edit-document.component.ts
@@ -14,7 +14,7 @@ import { SharedDocumentService } from '../Service/shared-document.service';
   selector: 'app-edit-document',
   templateUrl: './edit-document.component.html',
   styleUrls: ['./edit-document.component.css'],
-  providers: [MarkerService, DocumentService, DrawingService,SharedDocumentService]
+  providers: [MarkerService, DocumentService, DrawingService, SharedDocumentService]
 })
 export class EditDocumentComponent implements OnInit {
   circle = faCircleNotch
@@ -26,7 +26,7 @@ export class EditDocumentComponent implements OnInit {
   markerType: MarkerType
   color: string
   document: Document = new Document()
-  isShared:boolean
+  isShared: boolean
 
   image: any
   @Input() documentId: string
@@ -41,7 +41,7 @@ export class EditDocumentComponent implements OnInit {
     private route: ActivatedRoute,
     private documentService: DocumentService,
     private drawingService: DrawingService,
-    private sharingService:SharedDocumentService) {
+    private sharingService: SharedDocumentService) {
   }
 
   ngOnInit(): void {
@@ -49,56 +49,50 @@ export class EditDocumentComponent implements OnInit {
       this.documentId = params['documentId']
       this.documentService.getDocumentById(this.documentId)
       this.sharingService.getAllUsersForShare(this.documentId)
-
-      this.markerSerivce.onCreateMarkerResponseOk().subscribe(
-        response => {
-          this.notifications.showSuccess("Great!", "Success")
-          var ctx1 = this.shapeCanvas.nativeElement.getContext('2d')
-          let marker = response.request.marker
-          let position = JSON.parse(marker.position)
-          this.drawShapeOnCanvas(ctx1, position, marker.markerType, this.color)
-        }
-      )
-
-      this.markerSerivce.onGetMarkersResponseOk().subscribe(
-        response => {
-          this.drawMarkers(response)
-          var ctx1 = this.shapeCanvas.nativeElement.getContext('2d')
-          ctx1.strokeStyle = "Black"
-        }
-      )
+      this.initObservers();
     })
+  }
+
+  private initObservers() {
+    this.markerSerivce.onCreateMarkerResponseOk().subscribe(
+      response => {
+        this.notifications.showSuccess("Great!", "Success");
+        var ctx1 = this.shapeCanvas.nativeElement.getContext('2d');
+        let marker = response.request.marker;
+        let position = JSON.parse(marker.position);
+        this.drawShapeOnCanvas(ctx1, position, marker.markerType, this.color);
+      }
+    );
+
+    this.markerSerivce.onGetMarkersResponseOk().subscribe(
+      response => {
+        this.drawMarkers(response);
+        var ctx1 = this.shapeCanvas.nativeElement.getContext('2d');
+        ctx1.strokeStyle = "Black";
+      }
+    );
 
     this.documentService.onGetDocumentResponseOk().subscribe(
       result => {
-        this.document = result.doc
-        this.image = result.image
-        var ctx1 = this.shapeCanvas.nativeElement.getContext('2d')
-        this.buildImage(ctx1, this.shapeCanvas.nativeElement.width, this.shapeCanvas.nativeElement.height);
+        this.document = result.doc;
+        this.image = result.image;
+        this.buildImage(this.shapeCanvas.nativeElement.getContext('2d'));
       }
-    )
+    );
 
     this.sharingService.onGetAllUsersResponseOk().subscribe(
-      result=>{
-        this.isShared = Array.of(...result).filter(obj=>obj.isSharedWith).length>0
-      }
-    )
+      result => this.isShared = Array.of(...result).filter(obj => obj.isSharedWith).length > 0
+    );
   }
 
-  buildImage(ctx1: any, maxWidth: number, maxHeight: number) {
+  buildImage(ctx1: any) {
     var shapeCanvas = this.shapeCanvas.nativeElement
     var drawingCanvas = this.drawingCanvas.nativeElement
     let base_image = new Image()
     base_image.src = this.image
     base_image.onload = () => {
-      let width = base_image.width
-      let height = base_image.height
-      if (width <= maxWidth && height <= maxHeight) {
-        ctx1.drawImage(base_image, 0, 0)
-      } else {
-        this.setCanvasBoundaries(shapeCanvas, base_image.width, base_image.height, drawingCanvas);
-        ctx1.drawImage(base_image, 0, 0, base_image.width, base_image.height);
-      }
+      this.setCanvasBoundaries(shapeCanvas, base_image.width, base_image.height, drawingCanvas);
+      ctx1.drawImage(base_image, 0, 0, base_image.width, base_image.height);
       this.markerSerivce.getMarkers(this.documentId)
     }
   }
@@ -157,8 +151,7 @@ export class EditDocumentComponent implements OnInit {
 
   drawShapeOnCanvas(ctx1: any, position: any, markerType: MarkerType, color: any) {
     ctx1.beginPath()
-    ctx1.lineWidth = 2
-    ctx1.strokeStyle = color
+    this.configureLineParams(ctx1, color);
     if (markerType == MarkerType.Ellipse) {
       ctx1.ellipse(position.centerX, position.centerY, position.radiusX, position.radiusY, 0, 0, 2 * Math.PI)
     } else {
@@ -167,5 +160,10 @@ export class EditDocumentComponent implements OnInit {
       ctx1.rect(topLeftX, topLeftY, position.radiusX * 2, position.radiusY * 2)
     }
     ctx1.stroke()
+  }
+
+  private configureLineParams(ctx1: any, color: any, width: number = 2) {
+    ctx1.lineWidth = width;
+    ctx1.strokeStyle = color;
   }
 }
